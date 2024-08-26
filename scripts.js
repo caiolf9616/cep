9fdb47cb746a40dee7bc7ec3cc2fcee781830197
Caio Fernandes <133905605+caiolf9616@users.noreply.github.com>
2024-08-26 12:25:56 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,43 @@
+const end = document.querySelector('#endereco')
+const bairro = document.querySelector('#bairro')
+const cidade = document.querySelector('#cidade')
+const estado = document.querySelector('#estado')
+const cep = document.querySelector('#cep')
+
+function preencherEndereco(endereco){
+    bairro.value = endereco.bairro
+    cidade.value = endereco.localidade
+    estado.value = endereco.uf
+    end.value = endereco.logradouro
+
+}
+
+function isCepValid(cep){
+    return cep.length === 8 && /^[0-9]+$/.test
+}
+
+function buscarEndereco(){
+    const cep = document.querySelector('#cep').value
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    if (isCepValid(cep)){
+        fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(dados => {
+            if (dados .hasOwnProperty("erro")){
+                end.value = "Endereço não encontrado :/"
+            }
+            else{
+                preencherEndereco(dados)
+            }
+        })
+
+    }
+    else{
+        end.value = 'cep inválido!'
+    }
+
+}
+
+cep.addEventListener('blur', buscarEndereco)
